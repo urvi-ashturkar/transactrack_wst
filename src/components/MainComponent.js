26ab2e370cfda9fb36c3fd51aaca06a16f948ec1
Urvi Ashturkar <urvi.ashturkar@gmail.com>
2020-07-28 12:31:12 +0530

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,33 +1,39 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+
 class Main extends Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
       dishes: DISHES,
-      selectedDish: null
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId})
-  }
-
   render() {
+
+    const HomePage = () => {    //could also be done inline like Menu.
+      return(
+        <Home />
+      );
+    }
+
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes}
-          onClick={(dishId) => {this.onDishSelect(dishId);
-            console.log("clicked");
-            }} />
-        <Dishdetail
-          dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish )[0]} />
+        <Switch>
+          <Route path="/home" component={HomePage} />   //if url ending in "/home", render view of HomePage component.
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />    //Inline definition of component. Didn't say component={Menu} because wanted to pass props.
+          <Redirect to="/home" />   //if path does not match any of the above, go here.
+        </Switch>
         <Footer />
       </div>
     );
