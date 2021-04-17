84595ca7ec03a8ed8875a573a4355a1004c80d4a
Mugdha Bapat <mugdhabapat2000@gmail.com>
2021-04-17 18:13:18 +0530

    for teh sake of it

--- server/connection.js
+++ server/connection.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 const con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "password",
+  password: "elephant9",
   database: "teamDB",
   multipleStatements: true,
 });
